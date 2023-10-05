7b25387d5c5d30f4d87d569beab853587ea3bf95
adibb <sudibyoadib1504@gmail.com>
2023-10-06 02:16:57 +0700

    progress detail page 10%

--- lib/home page/detail_page.dart
+++ lib/home page/detail_page.dart
@@ -12,16 +12,94 @@ class DetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(mobil.nama),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Image.asset(mobil.gambar),
-        ),
+      body: Stack(
+        children: [
+          // Latar belakang biru
+          Positioned(
+            width: 400,
+            height: 184,
+            child: Container(
+              color: const Color(0xff16A6CC),
+            ),
+          ),
+          // Tombol "Back" di kiri atas
+          Positioned(
+            top: 50,
+            left: 33,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Color(0xff7ED8F1),
+                shape: BoxShape.circle, // Bentuk lingkaran
+              ),
+              child: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          ),
+          // Gambar dengan BorderRadius
+          Positioned(
+            top: 100,
+            left: 34,
+            right: 34,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 1.0,
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius:
+                    BorderRadius.circular(20), // BorderRadius untuk gambar
+                child: Image.asset(
+                  mobil.gambar,
+                  fit: BoxFit.fill,
+                  width: 321,
+                  height: 191,
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
+            top: 310,
+            left: 36,
+            right: 40,
+            child: RichText(
+              text: const TextSpan(
+                children: [
+                  TextSpan(
+                    text: '450.000',
+                    style: TextStyle(
+                      fontFamily: 'asset/fonts/Poppins-SemiBold.ttf',
+                      fontWeight: FontWeight.w600,
+                      color: Colors.green,
+                      fontSize: 28,
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: SizedBox(width: 5),
+                  ),
+                  TextSpan(
+                    text: '/day',
+                    style: TextStyle(
+                      fontFamily: 'asset/fonts/Poppins-Regular.ttf',
+                      color: Colors.black,
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
